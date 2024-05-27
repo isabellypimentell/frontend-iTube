f552860aa6feb3bb792b2bc5c476b6694a9a233d
isabellypimentell <126909881+isabellypimentell@users.noreply.github.com>
2024-05-27 19:14:08 +0000

    home

---
+++ my-app/src/app/home.css/page.jsx
@@ -0,0 +1,5 @@
+document.getElementById('search-button').addEventListener('click', function() {
+    var query = document.getElementById('search-input').value;
+    alert('Searching for: ' + query);
+    // Implement search functionality here
+});
